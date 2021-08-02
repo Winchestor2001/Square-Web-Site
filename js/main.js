3a878894e58c7228a568e80824ce067b0241a698
Winchestor2001 <sbexruz2001@gmail.com>
2021-08-02 15:54:32 +0500

    square-site

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+
+    
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplaySpeed: 800,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                dots: true,
+            },
+            600: {
+                items: 3,
+                nav: false,
+                dots: true,
+            },
+            1000: {
+                items: 4,
+                nav: false,
+                dots: true,
+            }
+        }
+    });
+    owl.on('drag.owl.carousel', function (event) {
+        document.ontouchmove = function (e) {
+            e.preventDefault()
+        }
+    })
+
+
+
+    $('.menu-btn').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-btn-active');
+    })
+
+
+
+
+    new WOW().init();
+});
+
